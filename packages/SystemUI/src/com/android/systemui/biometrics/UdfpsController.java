6d84ab61a9ba79575772577525d97c700e29447e
Musical143143 <basub4615@gmail.com>
2025-01-09 12:09:33 +0000

    Revert "SystemUI: UdfpsController: Fix UDFPS illumination"
    This reverts commit cfe58f2304268747fc10c83da020cdd2a8fe22fa.

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -356,6 +356,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         return;
                     }
                     mAcquiredReceived = true;
+                    final View view = mOverlay.getTouchOverlay();
+                    unconfigureDisplay(view);
+                    tryAodSendFingerUp();
                 });
             }
         }
